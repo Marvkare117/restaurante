b92cc56c585cc03942c617717e5800f1c22bac44
Marvkare <marvkare_games@yahoo.com>
2024-06-18 02:07:12 -0600

    initpush

---
+++ src/main/java/com/alexandertutoriales/ecommerce/service/App.java
@@ -0,0 +1,32 @@
+package com.alexandertutoriales.ecommerce.service;
+
+import com.alexandertutoriales.ecommerce.service.config.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
+public class App {
+    public static void main (String[] args){
+        SpringApplication.run(App.class, args);
+    
+    }
+    @Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
+}
